d9a1e87ff82392e467fd0b67c341b6b0e0fc676b
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-01-26 18:34:09 +0100

    Model USER V1

---
+++ project/database/models/User.js
@@ -0,0 +1,24 @@
+module.exports = class User {
+
+    constructor(id, firstname, lastname, password, email, phone, active, admin, profileImg, payment, address) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.active = active;
+        this.admin = admin;
+        this.profileImg = profileImg;
+        this.payment = payment;
+        this.address = address;
+    };
+
+    static UserUpdate(firstname, lastname, email, phone, check, address){
+        return new User(null, firstname, lastname, null, email, phone, null, null, null, null, address);
+    };
+
+    static UserInsert(firstname, lastname, password, email, active, admin, payment, address){
+        return new User(null, firstname, lastname, password, email,null, null, null, null, null, address)
+    };
+}
