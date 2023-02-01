99e511fd57d3f7c49b65b07b08f1c1fe00650ff1
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-01 18:07:41 +0100

    User DAO

--- project/database/dao/userDAO.js
+++ project/database/dao/userDAO.js
@@ -9,24 +9,41 @@ const errorMessage = "Data access error";
 
 const SQL_INSERT_USER = `INSERT INTO user SET firstname = ?, lastname = ?, password = ?, email = ?, id_payment = ?, id_address = ?`;
 const SQL_REMOVE_USER = `DELETE FROM user WHERE id = ?`;
-const SQL_UPDATE_PROFILE = `UPDATE user SET firstname = ?, lastname = ?, phone = ?, profile_img = ? WHERE id = ?`;
+const SQL_UPDATE_PROFILE = `UPDATE user SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?`;
 const SQL_UPDATE_PASSWORD = `UPDATE user SET password = ? WHERE id = ?`;
-const SQL_UPDATE_EMAIL = `UPDATE user SET email = ? WHERE id = ?`;
 const SQL_UPDATE_ACTIVE = `UPDATE user SET active = ? WHERE id = ?`;
 const SQL_UPDATE_ADMIN = `UPDATE user SET admin = ? WHERE id = ?`;
 const SQL_UPDATE_PROFILE_IMG = `UPDATE user SET profile_img = ? WHERE id = ?`;
 const SELECT_USER_BY_ID = `SELECT * FROM user WHERE id = ? `;
-const SELECT_ALL_USER = `SELECT * FROM user ORDER BY lastname DESC`;
+const SELECT_ALL_USER = `SELECT * FROM user ORDER BY lastname`;
+
+async function updateProfile(User, id){
+    let con = null;
+
+    try{
+        con = await database.getConnection();
+        await con.execute(SQL_UPDATE_PROFILE, [User.firstname, User.lastname, User.phone, User.email, id]);
+        const newUser = await getById(id);
+        return newUser;
+    }catch (error) {
+        log.error("Error userDAO update : " + error);
+        throw errorMessage;
+    } finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
 
 async function getById(id){
     let con = null;
     try{
         con = await database.getConnection();
         const [user] = await con.execute(SELECT_USER_BY_ID, [id]);
-        const payment = await paymentDAO.getById(user[0].id_payment);
+        let payment = await paymentDAO.getById(user[0].id_payment);
         const newPayment = new Payment(payment[0].id, payment[0].card_name, payment[0].card_number, payment[0].card_expired_date);
         const address = await addressDAO.getById(user[0].id_address);
-        return new User(user[0].id, user[0].firstname, user[0].lastname, user[0].password, user[0].email, user[0].phone, user[0].active, user[0].admin, user[0].profile_img, newPayment, address);
+        return new User(user[0].id, user[0].firstname, user[0].lastname, user[0].password, user[0].email, user[0].phone, user[0].active, user[0].admin, user[0].profileImg, newPayment, address);
     }catch (error) {
         log.error("Error userDAO getById : " + error);
         throw errorMessage;
@@ -37,6 +54,24 @@ async function getById(id){
     }
 }
 
+async function getAllUser(){
+    let con = null;
+    try{
+        con = await database.getConnection();
+        const [rows] = await con.execute(SELECT_ALL_USER);
+        return rows;
+    }catch (error) {
+        log.error("Error userDAO getAllUser : " + error);
+        throw errorMessage;
+    } finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
+
 module.exports = {
-    getById
+    updateProfile,
+    getById,
+    getAllUser,
 }
