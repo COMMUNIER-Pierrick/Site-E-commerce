dd3867d2d97e99f492f9b0cd808a8587fdae4a92
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-01 18:06:28 +0100

    Routes user v2

--- project/routes/user.js
+++ project/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controllers/user");
 
+router.get("/all-users", user.getAllUser);
 router.get("/:id", user.getById);
+router.put("/update-profile/:id", user.updateProfile);
 
 module.exports = router;
