08bb6a56b642f71cd9cfe3061198c63880c6f1c9
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-01 18:13:54 +0100

    Insert SQL

---
+++ resources/insert.sql
@@ -0,0 +1,73 @@
+INSERT INTO `category` SET category_name='Bagues';
+INSERT INTO `category` SET category_name='Boucles d\'oreilles';
+INSERT INTO `category` SET category_name='Bracelets';
+INSERT INTO `category` SET category_name='Colliers';
+INSERT INTO `category` SET category_name='Parrures';
+
+INSERT INTO `sub_category` SET sub_category_name='Petites';
+INSERT INTO `sub_category` SET sub_category_name='Moyennes';
+INSERT INTO `sub_category` SET sub_category_name='Grandes';
+INSERT INTO `sub_category` SET sub_category_name='Boucles d\'oreilles';
+INSERT INTO `sub_category` SET sub_category_name='Boucles d\'oreilles créoles';
+INSERT INTO `sub_category` SET sub_category_name='Clous d\'oreilles';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets ajustable';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets breloque / chaînette';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets cheville';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets cordon / cuir';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets élastique';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets perles';
+INSERT INTO `sub_category` SET sub_category_name='Bracelets homme';
+INSERT INTO `sub_category` SET sub_category_name='Collers chaîne';
+INSERT INTO `sub_category` SET sub_category_name='Colliers cordon';
+INSERT INTO `sub_category` SET sub_category_name='Colliers médaillon photo';
+INSERT INTO `sub_category` SET sub_category_name='Colliers ras de cou';
+INSERT INTO `sub_category` SET sub_category_name='Colliers sautoir';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble bague / boucles d\'oreilles';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble bague / bracelet';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble bague / boucles d\'oreilles / bracelet / collier';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble bracelet / collier';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble boucles d\'oreilles / bracelet';
+INSERT INTO `sub_category` SET sub_category_name='Ensemble boucles d\'oreilles / collier';
+
+INSERT INTO `status` SET status_name='Payement Accepté';
+INSERT INTO `status` SET status_name='En cours';
+INSERT INTO `status` SET status_name='Terminé';
+INSERT INTO `status` SET status_name='Envoyé';
+INSERT INTO `status` SET status_name='Reçu';
+
+INSERT INTO `address` SET number=31, street='rue du cheval', additional_address='', zipcode='84200', city='Carpentras', country='France';
+INSERT INTO `address` SET number=65, street='rue des moulins', additional_address='2ème étage', zipcode='23500', city='Poussanges', country='France';
+INSERT INTO `address` SET number=44, street='rue du chat', additional_address='', zipcode='42300', city='Mably', country='France';
+INSERT INTO `address` SET number=11, street='rue de la paix', additional_address='', zipcode='06500', city='Castillon', country='France';
+INSERT INTO `address` SET number=9, street='rue des roses', additional_address='5ème étage', zipcode='16100', city='Saint-Brice', country='France';
+INSERT INTO `address` SET number=2, street='rue de la loupe', additional_address='', zipcode='62600', city='Groffiers', country='France';
+
+INSERT INTO `payment` SET card_name='', card_number='', card_expired_date='';
+INSERT INTO `payment` SET card_name='Jean Dujardin', card_number='5632153652638652', card_expired_date='2025-10-15T00:00:00';
+INSERT INTO `payment` SET card_name='Marion Cotillard', card_number='7532641232597854', card_expired_date='2027-02-28T00:00:00';
+INSERT INTO `payment` SET card_name='', card_number='', card_expired_date='';
+INSERT INTO `payment` SET card_name='Avril Lavigne', card_number='9645236896411597', card_expired_date='2024-07-04T00:00:00';
+INSERT INTO `payment` SET card_name='', card_number='', card_expired_date='';
+
+INSERT INTO `user` SET firstname='Jean', lastname='Dujardin', password='test', email='Dujardin@gmail.com', phone='0683942366', active='1', admin='0', profile_img='no_picture.jpg', id_payment=2, id_address=3;
+INSERT INTO `user` SET firstname='Marion', lastname='Cotillard', password='test', email='Cotillard@gmail.com', phone='', active='1', admin='0', profile_img='no_picture.jpg', id_payment=3, id_address=6;
+INSERT INTO `user` SET firstname='Avril', lastname='Lavigne', password='test', email='Lavigne@gmail.com', phone='0687559314', active='1', admin='1', profile_img='no_picture.jpg', id_payment=5, id_address=1;
+INSERT INTO `user` SET firstname='Brad', lastname='Pitt', password='test', email='Pitt@gmail.com', phone='', active='1', admin='0', profile_img='no_picture.jpg', id_payment=1, id_address=5;
+INSERT INTO `user` SET firstname='Margot', lastname='Robbie', password='test', email='Robbie@gmail.com', phone='0232864192', active='1', admin='0', profile_img='no_picture.jpg', id_payment=6, id_address=2;
+INSERT INTO `user` SET firstname='George', lastname='Clooney', password='test', email='Clooney@gmail.com', phone='', active='1', admin='0', profile_img='no_picture.jpg', id_payment=4, id_address=4;
+
+INSERT INTO `product` SET product_name='Bague rose', id_category=1, id_sub_category=2, price=5, product_img='no_picture.png', description_product='', stock=4, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Bague bleue', id_category=1, id_sub_category=1, price=3, product_img='no_picture.png', description_product='', stock=2, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Boucles d\'oreilles plume noir', id_category=2, id_sub_category=4, price=6, product_img='no_picture.png', stock=7, description_product='', date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Boucles d\'oreilles plume vert', id_category=2, id_sub_category=4, price=6, product_img='no_picture.png', stock=4, description_product='', date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Boucles d\'oreilles plume violet', id_category=2, id_sub_category=4, price=6, product_img='no_picture.png', stock=3, description_product='', date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Bracelet noir', id_category=3, id_sub_category=7, price=4, product_img='no_picture.png', description_product='', stock=1, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Bracelet gris', id_category=3, id_sub_category=13, price=6, product_img='no_picture.png', description_product='', stock=12, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Bracelet rose', id_category=3, id_sub_category=9, price=5, product_img='no_picture.png', description_product='', stock=8, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Collier bleue', id_category=4, id_sub_category=14, price=10, product_img='no_picture.png', description_product='', stock=4, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Collier rouge', id_category=4, id_sub_category=15, price=7, product_img='no_picture.png', description_product='', stock=6, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Collier jaune', id_category=4, id_sub_category=16, price=8, product_img='no_picture.png', description_product='', stock=4, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Parrure turquoise', id_category=5, id_sub_category=19, price=17, product_img='no_picture.png', description_product='', stock=1, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Parrure blanche', id_category=5, id_sub_category=20, price=15, product_img='no_picture.png', description_product='', stock=9, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Parrure Multicolore', id_category=5, id_sub_category=22, price=22, product_img='no_picture.png', description_product='', stock=12, date_created='2022-10-15T00:00:00';
+INSERT INTO `product` SET product_name='Parrure orange', id_category=5, id_sub_category=24, price=12, product_img='no_picture.png', description_product='', stock=6, date_created='2022-10-15T00:00:00';
