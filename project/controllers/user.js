95aa0b2dbc838f5c363846a6e7331ee301b034c7
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-01 18:08:15 +0100

    Controller User

--- project/controllers/user.js
+++ project/controllers/user.js
@@ -1,4 +1,30 @@
 const userDAO = require("../database/dao/userDAO");
+const User = require("../database/models/User");
+const {updateValidation} = require("../services/validation");
+const log = require("./../log/logger");
+const fileDAO = require("../database/dao/fileDao");
+
+const updateProfile = async (req, res) => {
+    const {id} = req.params;
+    let userData = req.body.User;
+    const user = User.UserUpdateProfile(userData[0].firstname, userData[0].lastname, userData[0].email, userData[0].phone);
+
+    const { error } = updateValidation(user);
+    if(error){
+        log.error("Error update : " + error.details[0].message);
+        return res.status(400).send({ error: error.details[0].message });
+    }
+
+    try{
+        const newUser = await userDAO.updateProfile(user, id);
+        delete newUser.password;
+        const message = "mise à jour réussi.";
+        res.status(200).send( {"Message": message, "User": newUser} );
+    }catch (error) {
+        log.error("Error userDAO update");
+        throw error;
+    }
+};
 
 const getById = async (req, res) => {
     const {id} = req.params;
@@ -6,6 +32,13 @@ const getById = async (req, res) => {
     res.status(200).send( {"User": user} );
 };
 
+const getAllUser = async (req, res) => {
+    const user = await userDAO.getAllUser();
+    res.status(200).send( {"Users": user} );
+}
+
 module.exports = {
-    getById
+    updateProfile,
+    getById,
+    getAllUser,
 };
