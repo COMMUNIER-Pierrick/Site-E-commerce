60281f9e51d170379919d0354d51e50a3be98f98
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-07 17:37:10 +0100

    PaymentDAO

--- project/database/dao/paymentDAO.js
+++ project/database/dao/paymentDAO.js
@@ -5,11 +5,11 @@ const errorMessage = "Data access error";
 
 const SELECT_BY_ID = `SELECT * FROM payment WHERE id = ?`;
 
-async function getById(id_payment){
+async function getById(id){
     let con = null;
     try{
         con = await database.getConnection();
-        const [payment] = await con.execute(SELECT_BY_ID, [id_payment]);
+        const [payment] = await con.execute(SELECT_BY_ID, [id]);
         return payment;
     }catch (error) {
         log.error("Error paymentDAO getById : " + error);
