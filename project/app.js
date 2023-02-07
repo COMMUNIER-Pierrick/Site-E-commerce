25c803eee9ddcc4d8507772c30192ff514a3e592
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-07 17:33:22 +0100

    App

--- project/app.js
+++ project/app.js
@@ -7,6 +7,13 @@ const http = require("http").createServer(app);
 app.use(express.json());
 
 app.use("/user", require("./routes/user"));
+app.use("/address", require("./routes/address"));
+app.use("/category", require("./routes/category"));
+app.use("/sub-category", require("./routes/subCategory"));
+app.use("/status", require("./routes/status"));
+app.use("/product", require("./routes/product"));
+app.use("/basket", require("./routes/basket"));
+app.use("/payment", require("./routes/payment"));
 
 try{
     http.listen(port, () => {
