7eb1496b58b854be98b1fbf2c05a7f223cda62fd
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-07 17:39:25 +0100

    User controller

--- project/controllers/user.js
+++ project/controllers/user.js
@@ -2,14 +2,13 @@ const userDAO = require("../database/dao/userDAO");
 const User = require("../database/models/User");
 const {updateValidation} = require("../services/validation");
 const log = require("./../log/logger");
-const fileDAO = require("../database/dao/fileDao");
 
 const updateProfile = async (req, res) => {
     const {id} = req.params;
     let userData = req.body.User;
     const user = User.UserUpdateProfile(userData[0].firstname, userData[0].lastname, userData[0].email, userData[0].phone);
 
-    const { error } = updateValidation(user);
+    const {error} = updateValidation(user);
     if(error){
         log.error("Error update : " + error.details[0].message);
         return res.status(400).send({ error: error.details[0].message });
@@ -19,7 +18,7 @@ const updateProfile = async (req, res) => {
         const newUser = await userDAO.updateProfile(user, id);
         delete newUser.password;
         const message = "mise à jour réussi.";
-        res.status(200).send( {"Message": message, "User": newUser} );
+        res.status(200).send({"Message": message, "User": newUser});
     }catch (error) {
         log.error("Error userDAO update");
         throw error;
@@ -29,12 +28,12 @@ const updateProfile = async (req, res) => {
 const getById = async (req, res) => {
     const {id} = req.params;
     const user = await userDAO.getById(id);
-    res.status(200).send( {"User": user} );
+    res.status(200).send({"User": user});
 };
 
 const getAllUser = async (req, res) => {
     const user = await userDAO.getAllUser();
-    res.status(200).send( {"Users": user} );
+    res.status(200).send({"Users": user});
 }
 
 module.exports = {
