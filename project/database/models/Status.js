d6bbe55828e3335cbaf8a32a09b6aa7024780672
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2023-02-07 17:37:47 +0100

    Status model

--- project/database/models/Status.js
+++ project/database/models/Status.js
@@ -3,12 +3,4 @@ module.exports = class Status{
         this.id = id;
         this.statusName = statusName;
     };
-
-    static StatusInsert(statusName) {
-        return new Status(null, statusName);
-    };
-
-    static StatusUpdate(statusName) {
-        return new Status(null, statusName);
-    };
 }
