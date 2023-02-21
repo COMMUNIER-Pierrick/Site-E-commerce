cdc0679b4501ab2e5adf71abe4990cd011cd5d2f
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-02-21 18:16:27 +0100

    Address DAO

--- project/database/dao/addressDAO.js
+++ project/database/dao/addressDAO.js
@@ -8,12 +8,29 @@ const SQL_REMOVE_ADDRESS = `DELETE FROM address WHERE id = ?`;
 const SQL_UPDATE_ADDRESS = `UPDATE address SET number = ?, street = ?, additional_address = ?, zipcode = ?, city = ?, country = ? WHERE id = ?`;
 const SELECT_ADDRESS_BY_ID = `SELECT * FROM address WHERE id = ? `;
 
+async function insert(Address){
+    let con = null;
+    try{
+        con = await database.getConnection();
+        const [addressCreated] = await con.execute(SQL_INSERT_ADDRESS, [Address.number, Address.street, Address.additionalAddress, Address.zipCode, Address.city, Address.country]);
+        const id = addressCreated.insertId;
+        const [address] = await getById(id);
+        return address;
+    }catch (error){
+        log.error("Error addressDAO insert : " + error);
+        throw errorMessage;
+    }finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
 async function getById(id){
     let con = null;
     try {
         con = await database.getConnection();
-        const [rows] = await con.execute(SELECT_ADDRESS_BY_ID, [id]);
-        return rows;
+        const [address] = await con.execute(SELECT_ADDRESS_BY_ID, [id]);
+        return address;
     } catch (error) {
         log.error("Error addressDAO getById : " + error);
         throw errorMessage;
@@ -25,5 +42,6 @@ async function getById(id){
 }
 
 module.exports = {
-    getById
+    insert,
+    getById,
 }
