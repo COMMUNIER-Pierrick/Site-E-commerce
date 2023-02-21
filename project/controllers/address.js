a9584a535abd30c8331635d4c8d731f10d6086cb
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-02-21 18:15:31 +0100

    Address controller

--- project/controllers/address.js
+++ project/controllers/address.js
@@ -1,5 +1,13 @@
 const addressDAO = require("../database/dao/addressDAO");
+const Address = require("../database/models/Address");
+const log = require("../log/logger");
 
+const insert = async (req, res) => {
+    const {number, street, additionalAddress, zipCode, city, country} = req.body.Address;
+    const newAddress = Address.AddressInsert(number, street, additionalAddress, zipCode, city, country);
+    const address = await addressDAO.insert(newAddress);
+    res.status(200).send({"Address": address});
+}
 
 const getById = async (req, res) => {
     const {id} = req.params;
@@ -8,5 +16,6 @@ const getById = async (req, res) => {
 };
 
 module.exports = {
+    insert,
     getById,
 };
