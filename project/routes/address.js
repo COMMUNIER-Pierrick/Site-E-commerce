9764940753d7a103003d4979fd6f873c451835da
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-14 19:12:39 +0100

    Address routes

--- project/routes/address.js
+++ project/routes/address.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const address = require("../controllers/address");
 
-router.post("/add-address", address.insert);
-
-router.get("/:id", address.getById);
+router.put("/update/:id", address.update);
 
 module.exports = router;
