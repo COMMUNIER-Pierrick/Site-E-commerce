c2a61f3a5d4ee057299d6e2f38f7085b8c1cc463
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-14 19:11:18 +0100

    Payment model

--- project/database/models/Payment.js
+++ project/database/models/Payment.js
@@ -1,8 +1,12 @@
 module.exports = class Payment{
-    constructor(id, cardName, cardNumber, cardExpiredDate) {
+    constructor(id, card_name, card_number, card_expired_date) {
         this.id = id;
-        this.cardName = cardName;
-        this.cardNumber = cardNumber;
-        this.cardExpiredDate = cardExpiredDate;
+        this.card_name = card_name;
+        this.card_number = card_number;
+        this.card_expired_date = card_expired_date;
+    };
+
+    static PaymentUpdate(id, card_name, card_number, card_expired_date){
+        return new Payment(id, card_name, card_number, card_expired_date)
     };
 }
