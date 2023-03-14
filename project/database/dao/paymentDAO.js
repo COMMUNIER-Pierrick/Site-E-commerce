b6f5ea8039e2091d07df6438b1bf806bcef1acc1
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-14 19:13:39 +0100

    PaymentDAO

--- project/database/dao/paymentDAO.js
+++ project/database/dao/paymentDAO.js
@@ -3,7 +3,56 @@ const log = require("../../log/logger");
 
 const errorMessage = "Data access error";
 
+const SQL_INSERT_PAYMENT = `INSERT INTO payment SET card_name = ?, card_number = ?, card_expired_date = ?`;
+const SQL_DELETE_PAYMENT = `DELETE FROM payment WHERE id = ?`;
+const SQL_UPDATE_PAYMENT = `UPDATE payment SET card_name = ?, card_number = ?, card_expired_date = ? WHERE id = ?`;
 const SELECT_BY_ID = `SELECT * FROM payment WHERE id = ?`;
+
+async function insert(){
+    let con = null;
+    try{
+        con = await database.getConnection();
+        const [paymentCreated] = await con.execute(SQL_INSERT_PAYMENT, ['','','']);
+        return paymentCreated.insertId;
+    }catch (error){
+        log.error("Error paymentDAO insert : " + error);
+        throw errorMessage;
+    }finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
+
+async function remove(id){
+    let con = null;
+    try{
+        con = await database.getConnection();
+        await con.execute(SQL_DELETE_PAYMENT, [id]);
+    }catch (error) {
+        log.error("Error paymentDAO delete : " + error);
+        throw errorMessage;
+    } finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
+
+async function update(Payment){
+    let con = null;
+    try{
+        con = await database.getConnection();
+        await con.execute(SQL_UPDATE_PAYMENT, [Payment.card_name, Payment.card_number, Payment.card_expired_date, Payment.id]);
+    }catch (error){
+        log.error("Error paymentDAO update : " + error);
+        throw errorMessage;
+    }finally {
+        if (con !== null) {
+            con.end();
+        }
+    }
+}
 
 async function getById(id){
     let con = null;
@@ -22,5 +71,8 @@ async function getById(id){
 }
 
 module.exports = {
+    insert,
+    remove,
+    update,
     getById
 }
