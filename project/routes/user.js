49d83199c980959f1b842f8dc60d024c6c9c0809
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-14 19:12:59 +0100

    User routes

--- project/routes/user.js
+++ project/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controllers/user");
 
+router.post("/register", user.insert);
+router.delete("/remove-user/:id", user.remove);
+
 router.get("/all-users", user.getAllUser);
 router.get("/:id", user.getById);
 router.put("/update-profile/:id", user.updateProfile);
