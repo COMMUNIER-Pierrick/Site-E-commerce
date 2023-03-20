49613cbca9010b5c14cd7198e3486ebd9a430f57
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-20 18:06:13 +0100

    Category routes

--- project/routes/category.js
+++ project/routes/category.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const category = require("../controllers/category");
 
+router.post("/add-new-category", category.insert);
+router.delete("/remove-category/:id", category.remove);
+
 router.get("/all-categories", category.getAllCategories);
 router.get("/:id", category.getById);
+router.put("/update-category/:id", category.update);
 
 module.exports = router;
