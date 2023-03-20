94771995142c1f7e0fff541428061c39bb88ca5d
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-20 18:06:22 +0100

    Product routes

--- project/routes/product.js
+++ project/routes/product.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const product = require("../controllers/product");
 
+router.post("/add-new-product", product.insert);
+
 router.get("/all-products", product.getAllProducts);
-router.get("/:id", product.getById);
+router.get("/all-products-by-product-name", product.getAllProductsByProductName);
+router.get("/all-products-by-category/:id", product.getAllProductsByIdCategory);
+router.get("/all-products-by-subcategory/:id", product.getAllProductsByIdSubcategory);
 
 module.exports = router;
