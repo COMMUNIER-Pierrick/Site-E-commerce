5eb249495500e4164fc25ab7ed62bb71ddb09ac6
COMMUNIER-Pierrick <pierrick02732@gmail.com>
2023-03-20 18:04:33 +0100

    Product model

--- project/database/models/Product.js
+++ project/database/models/Product.js
@@ -1,9 +1,9 @@
 module.exports = class Product{
-    constructor(id, productName, Category, SubCategory, price, productImg, descriptionProduct, stock, dateCreated) {
+    constructor(id, productName, Category, Subcategory, price, productImg, descriptionProduct, stock, dateCreated) {
         this.id = id;
         this.productName = productName;
         this.Category = Category;
-        this.SubCategory = SubCategory;
+        this.Subcategory = Subcategory;
         this.price = price;
         this.productImg = productImg;
         this.descriptionProduct = descriptionProduct;
@@ -11,11 +11,11 @@ module.exports = class Product{
         this.dateCreated = dateCreated;
     };
 
-    static ProductUpdate(id, productName, Category, SubCategory, price, productImg, descriptionProduct, stock){
+    static ProductUpdate(id, productName, Category, Subcategory, price, productImg, descriptionProduct, stock){
         return new Product(id, productName, null, null, price, productImg, descriptionProduct, stock);
     };
 
-    static ProductInsert(productName, Category, SubCategory, price, productImg, descriptionProduct, stock){
-        return new Product(null, productName, Category, SubCategory, price, productImg, descriptionProduct, stock);
+    static ProductInsert(productName, Category, Subcategory, price, productImg, descriptionProduct, stock){
+        return new Product(null, productName, Category, Subcategory, price, productImg, descriptionProduct, stock);
     };
 }
